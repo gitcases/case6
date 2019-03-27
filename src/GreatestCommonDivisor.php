3346cc5e14d02b6ad8b3bf02868d07fa76ef669c
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-03-27 15:38:31 +0100

    Add GreateCommonDivisor class.

---
+++ src/GreatestCommonDivisor.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace drupol\case6;
+
+class GreatestCommonDivisor
+{
+    /**
+     * Get the divisors of a given number.
+     *
+     * @param int $num
+     *   The number.
+     * @param int $start
+     *   The number to start from.
+     *
+     * @return array
+     *   The divisors of the number.
+     */
+    public function factors($num, $start = 2)
+    {
+        $return = [1, $num];
+
+        $end = \ceil(\sqrt($num)) + 1;
+
+        for ($i = $start; $i < $end; ++$i) {
+            if (0 !== $num % $i) {
+                continue;
+            }
+
+            $return[$i] = $i;
+            $return[$num / $i] = $num / $i;
+        }
+
+        \asort($return);
+
+        return \array_values($return);
+    }
+
+    /**
+     * Get the greatest common divisor.
+     *
+     * @param int ...$x
+     *   The numbers.
+     *
+     * @return false|int
+     *   The greatest common divisor.
+     */
+    public function gcd(...$x)
+    {
+        $x = \array_map([$this, 'factors'], $x);
+
+        $intersect = \array_intersect(...$x);
+
+        return \end($intersect);
+    }
+}
